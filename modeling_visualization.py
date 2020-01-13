14b9e1914a57de75d2caaaeea7d8da29d9709bef
bitnurihan <48513588+bitnurihan@users.noreply.github.com>
2020-01-13 14:37:35 +0900

    Update modeling_visualization.py

--- modeling_visualization.py
+++ modeling_visualization.py
@@ -7,24 +7,24 @@ from scipy.optimize import curve_fit
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-def GRPs(x, grp_const, grp_slope):
+def GRPs(x, grp_const, grp_slope):  # calculating grps by cost
     g = np.around(grp_const + grp_slope * np.log(x), decimals=100)
     GRP = np.exp(g)
     return GRP
 
 
-def reach1(GRP, r1_const, r1_slope):
+def reach1(GRP, r1_const, r1_slope):  # calculating reach1+% by grps(impression)
     r = np.around(r1_const + r1_slope * np.log(GRP), decimals=100)
     reach = np.exp(r)
     return (reach/(1+reach))*100
 
 
-def visualization_of_final_model_by_r():
+def visualization_of_final_model():
     n = 500
     cost_grp = np.empty(n)
     grp_r1 = np.empty(n)
     modeling_data = []
-    cost = np.linspace(1,5000000000,500)
+    cost = np.linspace(1,5000000000,n)
     # [cost to grp, grp to reach]
     screen = (df_2s_ad_data['screen'].drop_duplicates()).values
     for s in screen:
@@ -53,9 +53,11 @@ def visualization_of_final_model_by_r():
     plt.close()
 
 
-df_2s_ad_data = pd.read_csv(r'C:\Users\hanbi01\Desktop\한빛누리\제일기획\TAR\modeling_data\modeling_data_2S.csv', encoding='ms949')
-df_2s_max_reach = pd.read_excel(r'C:\Users\hanbi01\Desktop\한빛누리\제일기획\TAR\modeling_data\modeling_data_2S.xlsx')
-df_2s_modeling = pd.read_excel(r'C:\Users\hanbi01\Desktop\한빛누리\제일기획\TAR\modeling_data\Cheil3A_DS2_190826 - Copy.xlsx')
+df_2s_ad_data = pd.read_csv('modeling_data_2S.csv', encoding='ms949')  # raw data for modeling
+df_2s_max_reach = pd.read_excel('modeling_data_2S.xlsx') # raw data for modeling
+df_2s_modeling = pd.read_excel('Cheil3A_DS2_190826.xlsx') # constanat & slop of each target and screen
+
+# preprocessing
 df_2s_modeling['AGE_CD'] = df_2s_modeling['AGE_CD'].astype(str)
 df_2s_modeling['Target'] = df_2s_modeling['GENDER_CD'].astype(str) + df_2s_modeling['AGE_CD'].str.zfill(4)
 df_2s_modeling = df_2s_modeling.set_index('Target')
@@ -67,11 +69,11 @@ df_2s_ad_data = df_2s_ad_data[['Month','age', 'screen', 'r1', 'GRP', 'impression
 df_2s_max_reach = df_2s_max_reach[['Month','age', 'screen', 'r1', 'GRP', 'impression', 'Cost', 'CPRP']]
 df_2s_ad_data['cpm'] = (df_2s_ad_data['Cost'] / df_2s_ad_data['impression'])*1000
 df_2s_max_reach['cpm'] = (df_2s_max_reach['Cost'] / df_2s_max_reach['impression'])*1000
-ages = (df_2s_ad_data['age'].drop_duplicates()).values
+targets = (df_2s_ad_data['age'].drop_duplicates()).values
 
-with PdfPages('1_test.pdf') as pdf_pages:
-    for age in ages:
+with PdfPages('modeling_visualization.pdf') as pdf_pages:
+    for target in targets: # modeling graph is showed by target
         df_2s_ad_data_age = df_2s_ad_data[df_2s_ad_data['age'] == age].copy()
         df_2s_max_reach_age = df_2s_max_reach[df_2s_max_reach['age'] == age].copy()
         df_2s_ad_data_total = pd.concat([df_2s_ad_data_age, df_2s_max_reach_age])
-        non_linear_regression()
+        visualization_of_final_model()
